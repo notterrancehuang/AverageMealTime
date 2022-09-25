17e95f80733d9ffea18b89e560552894c1f1fa43
Terrance Huang <notterrancehuang@gmail.com>
2022-09-25 17:11:31 -0400

    made check username and password functions not static again

--- login.py
+++ login.py
@@ -1,10 +1,12 @@
 from credentials_database import CredentialsDatabase
 
+
 class Login:
     def __init__(self, input_username, input_password):
         self.input_username = input_username
         self.input_password = input_password
 
     def check_validity(self):
-        CredentialsDatabase.check_valid_username(self.input_username) and \
-        CredentialsDatabase.check_valid_password(self.input_password)
+        credentials_database = CredentialsDatabase("users.db")
+        credentials_database.check_valid_username(self.input_username) and \
+            credentials_database.check_valid_password(self.input_password)
