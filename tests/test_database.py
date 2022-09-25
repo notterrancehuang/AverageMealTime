3587e3ac24a41588fffe5e342467f5d45c99860c
Terrance Huang <notterrancehuang@gmail.com>
2022-09-25 19:10:02 -0400

    fixed test files

--- tests/test_database.py
+++ tests/test_database.py
@@ -3,11 +3,11 @@ from my_time import MyTime
 
 
 def test():
-    start = MyTime(10, 30)
-    end = MyTime(11, 00)
+    start_time = MyTime(10, 30)
+    end_time = MyTime(11, 00)
     database_file = "times.db"
     db = Database(database_file)
     db.create_table()
-    db.insert_data(start, end)
+    db.insert_data("Terrance", start_time, end_time)
     db.get_times()
     db.close()
