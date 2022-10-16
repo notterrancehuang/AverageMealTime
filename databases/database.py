ee1f0354afdb1ca0e9d4f303df653a0c704da068
Minhack2020 <gw5962@rit.edu>
2022-10-16 17:51:26 -0400

    added function

--- databases/database.py
+++ databases/database.py
@@ -43,3 +43,5 @@ class Database:
         self.c.execute("SELECT * FROM times")
         print(self.c.fetchall())
 # create delete data function here
+    def delete_data(self,username: str):
+        self.c.execute("DELETE FROM times WHERE username = ?", (str,))
